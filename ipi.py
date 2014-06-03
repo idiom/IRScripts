13c350ef6b61a9a3a09c48622c0653688855803f
idiom <idiom@users.noreply.github.com>
2014-06-02 20:45:27 -0400

    Moved functions into IPInfo class

--- ipi.py
+++ ipi.py
@@ -7,100 +7,110 @@ import json
 import datetime
 
 
-def querydshield(ip):
-    result = None
-    try:
-        response = urllib2.urlopen('https://isc.sans.edu/api/ip/%s?json' % ip )
-        data = json.loads(response.read())['ip']
-        return data
-    except:
-        return 'An error occured...'
-
-def queryberkley(ip):
-    resultset = {'exists':False,'lasttime':''}
-    exists = False
-    response = urllib2.urlopen('https://security.berkeley.edu/aggressive_ips/ips')
-    for line in response:
-        if ip in line:
-            resultset['exists'] = True
-            ldat = line.split(' ')
-            resultset['lasttime'] = (datetime.datetime.fromtimestamp(int(ldat[-1])).strftime('%Y-%m-%d'))
-            resultset
-            break
-    return resultset
+class IPInfo:
     
-def queryblde(ip):
-    #check blocklist.de
-    exists = False
-    response = urllib2.urlopen('http://lists.blocklist.de/lists/all.txt')
-    for line in response:
-        if ip in line:
-            exists = True
-            break
-    return exists
-
-def queryashun(ip):
-    resultset = {'exists':False,'updated':'','comment':''}
-    exists = False
-    response = urllib2.urlopen('http://www.autoshun.org/files/shunlist.csv')
-    for line in response:
-        if ip in line:
-            resultset['exists'] = True
-            ldat = line.split(',')
-            resultset['updated'] = ldat[1]
-            resultset['comment'] = ldat[2]
-            break
-    return resultset
+    def __init__(self,ip):
+        self.ip = ip
     
-def queryethreats(ip):
-    exists = False
-    response = urllib2.urlopen('http://rules.emergingthreats.net/blockrules/')
-    for line in response:
-        if ip in line:
-            exists = True
-            break
-    return exists
-
-def querymalcode(ip):
-    exists = False
-    request = urllib2.build_opener()
-    request = urllib2.Request('http://malc0de.com/bl/IP_Blacklist.txt')
-    request.add_header('User-Agent','Mozilla/5.0')
-    response = urllib2.urlopen(request)
-    for line in response:
-        if ip in line:
-            exists = True
-            break
-    return exists
+    def __sendrequest(self,url):
+        request = urllib2.build_opener()
+        request = urllib2.Request(url)
+        request.add_header('User-Agent','Mozilla/5.0 (Windows NT 6.2; Win64; x64; rv:27.0) Gecko/20121011 Firefox/27.0')
+        response = urllib2.urlopen(request)
+        return response
     
-def queryspamhausbl(ip):
-    exists = False
-    request = urllib2.build_opener()
-    request = urllib2.Request('http://www.spamhaus.org/drop/drop.txt')
-    request.add_header('User-Agent','Mozilla/5.0')
-    response = urllib2.urlopen(request)
-    for line in response:
-        if ip in line:
-            exists = True
-            break
-    return exists
+    def querydshield(self):
+        result = None
+        try:
+            response = self.__sendrequest('https://isc.sans.edu/api/ip/%s?json' % self.ip)
+            data = json.loads(response.read())['ip']
+            return data
+        except:
+            return 'An error occured...'
+
+    def queryberkley(self):
+        resultset = {'exists':False,'lasttime':''}
+        exists = False
+        response = self.__sendrequest('https://security.berkeley.edu/aggressive_ips/ips')
+        for line in response:
+            if self.ip in line:
+                resultset['exists'] = True
+                ldat = line.split(' ')
+                resultset['lasttime'] = (datetime.datetime.fromtimestamp(int(ldat[-1])).strftime('%Y-%m-%d'))
+                resultset
+                break
+        return resultset
+        
+    def queryblde(self):
+        #check blocklist.de
+        exists = False
+        response = self.__sendrequest('http://lists.blocklist.de/lists/all.txt')
+        for line in response:
+            if self.ip in line:
+                exists = True
+                break
+        return exists
+
+    def queryashun(self):
+        resultset = {'exists':False,'updated':'','comment':''}
+        exists = False
+        response = self.__sendrequest('http://www.autoshun.org/files/shunlist.csv')
+        for line in response:
+            if self.ip in line:
+                resultset['exists'] = True
+                ldat = line.split(',')
+                resultset['updated'] = ldat[1]
+                resultset['comment'] = ldat[2]
+                break
+        return resultset
+        
+    def queryethreats(self):
+        exists = False
+        response = self.__sendrequest('http://rules.emergingthreats.net/blockrules/')
+        for line in response:
+            if self.ip in line:
+                exists = True
+                break
+        return exists
+
+    def querymalcode(self):
+        exists = False
+        response = self.__sendrequest('http://malc0de.com/bl/IP_Blacklist.txt')
+        for line in response:
+            if self.ip in line:
+                exists = True
+                break
+        return exists
+        
+    def queryspamhausbl(self):
+        exists = False
+        response = self.__sendrequest('http://www.spamhaus.org/drop/drop.txt')
+        for line in response:
+            if self.ip in line:
+                exists = True
+                break
+        return exists
    
 def main():
+    
     parser = argparse.ArgumentParser(description="Query services for information about an IP Address")
     parser.add_argument("ip", help="The target IP address")    
     parser.add_argument('-d','--debug',dest='debug',action='store_true',help="Print debug information")
     args = parser.parse_args()
+    
+    ip = IPInfo(args.ip)
+    
     print
     print
     print 'Querying DShield'
-    data = querydshield(args.ip)
+    data = ip.querydshield()
     print '-'*32 
     for key in data.keys():
         print '  {:15} {:15}'.format(key.capitalize(),data[key])
     print '-'*32  
     print
     print 'Checking Berkley Agressive IPs'
-    data = queryberkley(args.ip)
+    data = ip.queryberkley()
     print '-'*32  
     print '  {:15} {:15}'.format('Identified',str(data['exists']))
     if data['exists']:
@@ -108,13 +118,13 @@ def main():
     print '-'*32 
     print
     print 'Checking blocklist.de'
-    data = queryblde(args.ip)
+    data = ip.queryblde()
     print '-' * 32
     print '  {:15} {:15}'.format('Identified',str(data))
     print '-' * 32
     print
     print 'Checking autoshun'
-    data = queryashun(args.ip)
+    data = ip.queryashun()
     print '-' * 32
     print '  {:15} {:15}'.format('Identified',str(data['exists']))
     if data['exists']:
@@ -122,19 +132,19 @@ def main():
         print '  {:15} {:15}'.format('Comment',data['comment'])
     print
     print 'Checking Emerging Threats'
-    data = queryethreats(args.ip)
+    data = ip.queryethreats()
     print '-' * 32
     print '  {:15} {:15}'.format('Identified',str(data))
     print '-' * 32
     print
     print 'Checking malc0de'
-    data = querymalcode(args.ip)
+    data = ip.querymalcode()
     print '-' * 32
     print '  {:15} {:15}'.format('Identified',str(data))
     print '-' * 32
     print
     print 'Checking spamhaus Blacklist'
-    data = queryspamhausbl(args.ip)
+    data = ip.queryspamhausbl()
     print '-' * 32
     print '  {:15} {:15}'.format('Identified',str(data))
     print '-' * 32
